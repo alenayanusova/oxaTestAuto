ccb3154e923007cbedb55e9d8878f9a836af52e2
alena <alena.manuilenko@gmail.com>
2015-11-25 17:41:08 +0300

    base classes

---
+++ src/test/java/base/BasePage.java
@@ -0,0 +1,27 @@
+package base;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by yanusovaaa on 11/25/2015.
+ */
+public class BasePage {
+    protected WebDriver driver;
+    protected Logger log = Logger.getLogger(this.getClass().getName());
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+}
---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,32 @@
+package base;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by yanusovaaa on 11/25/2015.
+ */
+public class BaseTest {
+    protected WebDriver driver;
+    protected Logger log = Logger.getLogger(this.getClass().getName());
+
+    @Before
+    public void setUp() {
+        driver = new FirefoxDriver();
+        log.info("FirefoxDriver initialized");
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        log.info("Set timeout fro 3 seconds");
+        driver.get("http://www.google.by/");
+    }
+
+    @After
+    public void shutDown() {
+        driver.quit();
+        log.info("Driver shut down");
+    }
+}
