2d826320039ce71b930cf4ce86bf50a087c6ff3e
sivenkovdn <sivenkovdn@gmail.com>
2015-12-07 16:11:13 +0300

    ContactUsTest with try/catch construction Implementing pages initialization method in according to PageFactory

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -5,6 +5,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import pages.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,15 +14,35 @@ import java.util.concurrent.TimeUnit;
  * @author yanusovaaa
  */
 
-public class BaseTest {
+public abstract class BaseTest {
     protected WebDriver driver;
     protected Logger log = Logger.getLogger(this.getClass().getName());
     protected final String BASE_URL = "http://redesign-qa.oxagile.com/";
+
+    public AboutUsPage aboutUsPage;
+    public ContactUsPage contactUsPage;
+    public CustomSoftwareDevelopmentServices customSoftwareDevelopmentServices;
+    public JavaPage javaPage;
+    public MobileDevelopmentServices mobileDevelopmentServices;
+    public MobilePage mobilePage;
+    public NetPage netPage;
+    public NewsPage newsPage;
+    public OxaHomePage oxaHomePage;
+    public PhpPage phpPage;
+    public PortfolioPage portfolioPage;
+    public SearchResultsPage searchResultsPage;
+    public ServiciesPage serviciesPage;
+    public SoftwareQualityAssurance softwareQualityAssurance;
+    public TechnicalSupportAndMaintenancePage technicalSupportAndMaintenancePage;
+    public WebDevelopmentServices webDevelopmentServices;
+
+    public abstract void initPages();
 
 
     @Before
     public void setUp() {
         driver = new FirefoxDriver();
+        initPages();
         log.info("FirefoxDriver initialized");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         log.info("Set timeout fro 10 seconds");
