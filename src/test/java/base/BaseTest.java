4de7f2e03d12559a3be8b7175cde50eb50c45fdc
sivenkovdn <sivenkovdn@gmail.com>
2015-12-23 14:50:33 +0300

    XML Launchers and cross-browser testing

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,11 +2,17 @@ package base;
 
 import org.apache.log4j.Logger;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import pages.*;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -39,12 +45,26 @@ public abstract class BaseTest {
 
     public abstract void initPages();
 
-
+    @Parameters("browser")
     @BeforeMethod
-    public void setUp() {
-        driver = new FirefoxDriver();
+    public void setUp(String browser) {
+       // driver = new FirefoxDriver();
+        if (browser.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("ie")) {
+            File file = new File("C:\\IEDriverServer.exe");
+            System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
+            DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+            driver = new InternetExplorerDriver(capabilities);
+        }
+        else {
+            throw new IllegalArgumentException("The Browser Type is Undefined");
+        }
         initPages();
-        log.info("FirefoxDriver initialized");
+
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         log.info("Set timeout fro 10 seconds");
         driver.get(BASE_URL);
