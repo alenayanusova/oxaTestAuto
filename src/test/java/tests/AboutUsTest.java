de9a3158ec079397c6dfa07576fabfd87dac0082
alena <alena.manuilenko@gmail.com>
2015-12-23 18:16:45 +0300

    commit for allure reporter

--- src/test/java/tests/AboutUsTest.java
+++ src/test/java/tests/AboutUsTest.java
@@ -5,6 +5,11 @@ import org.junit.Assert;
 import org.testng.annotations.*;
 import org.openqa.selenium.support.PageFactory;
 import pages.*;
+import ru.yandex.qatools.allure.annotations.Description;
+import ru.yandex.qatools.allure.annotations.Features;
+import ru.yandex.qatools.allure.annotations.Severity;
+import ru.yandex.qatools.allure.annotations.Title;
+import ru.yandex.qatools.allure.model.SeverityLevel;
 
 /**
  * Created by yanusovaaa on 11/25/2015.
@@ -47,50 +52,73 @@ public class AboutUsTest extends BaseTest {
 
     }
 
-    @Test(enabled = false, description = "1) go to home page; \n"
+    @Features("About Us")
+    @Severity(SeverityLevel.BLOCKER)
+    @Title("navigation to about us tab")
+    @Description("1) go to home page; \n"
             + "2) go to about us; \n"
             + "3) check that page is about us. \n")
-    public void test1(){
+
+    @Test(enabled = true)
+    public void goToAboutUs(){
         log.info("Log step 1: Go to About Us tab");
         oxaHomePage.goToTabAboutAs();
         log.info("Log step 2: Check that page is About As");
+        makeScreenshot();
         Assert.assertEquals("page isn't about us", AboutUsPage.ABOUT_AS_HEADER_TEXT, aboutUsPage.getHeaderText());
 
     }
 
-    @Test (enabled = false, description = "1) go to home page; \n"
+    @Features("About Us")
+    @Severity(SeverityLevel.MINOR)
+    @Title("navigation to the news page")
+    @Description("1) go to home page; \n"
             + "2) go to news; \n"
-            + "3) check that page is news")
-    public void test2(){
+            + "3) check that page is news. \n")
+
+    @Test (enabled = true)
+    public void navigationToNewsPage(){
         log.info("Log step 1: Go to About As tab");
 
         oxaHomePage.goToTabAboutAs();
         log.info("Log step 2: Go to News");
         oxaHomePage.goToNewsPageFromAboutUsMenu();
-        log.info("Log step 3: Check that page is News");
+        log.info("Log step 3: Check that page is News and get screenshot");
+        makeScreenshot();
         Assert.assertEquals("page isn't news", NewsPage.NEWS_HEADER_TEXT, newsPage.getHeaderText());
     }
 
-    @Test(dataProvider = "requiredFieldsValidation", enabled = false, description = "1) go to home page; \n"
+    @Features("About Us")
+    @Severity(SeverityLevel.NORMAL)
+    @Title("check required fields for contact us form")
+    @Description("1) go to home page; \n"
             + "2) open contact us from about us menu by hover; \n"
             + "3) check validation for all objects. \n")
-    public void test3(String name, String email, String text, String expected) {
+
+    @Test(dataProvider = "requiredFieldsValidation", enabled = true)
+    public void checkValidationForContact(String name, String email, String text, String expected) {
         log.info("Log step 1: Go to Contact Us for About Us menu");
         oxaHomePage.goToContactUsFromAboutUsMenu();
 
 
         log.info("log step 2: Check for " + expected + " validation");
         contactUsPage.sendRequest(name, email, text);
+        makeScreenshot();
         Assert.assertEquals("validation message wasn't found", expected, contactUsPage.getValidationText(name, email, text));
 
     }
 
-    @Test (enabled = false, description = "1) go to home page; \n"
+    @Features("About Us")
+    @Severity(SeverityLevel.MINOR)
+    @Title("check navigation between pages")
+    @Description("1) go to home page; \n"
             + "2) go to about us; \n"
             + "3) go to QA page; \n"
             + "4) go to contact us; \n"
-            + "5) check that page is contct us. \n")
-    public void test4(){
+            + "5) check that page is contact us. \n")
+
+    @Test (enabled = true)
+    public void checkNavigationBetweenPages1(){
         log.info("Log step 1: Go to About As tab");
         oxaHomePage.goToTabAboutAs();
 
@@ -101,26 +129,39 @@ public class AboutUsTest extends BaseTest {
         softwareQualityAssurance.goToContactUs();
 
         log.info("Log step 4: Check that page is Contact Us");
+        makeScreenshot();
         Assert.assertEquals("page isn't contact us", ContactUsPage.CONTACT_US_HEADER_TEXT, contactUsPage.getHeaderText());
     }
 
-    @Test (dataProvider = "keysForSearch", enabled = false, description = "1) open hame page; \n"
+
+    @Features("About Us")
+    @Severity(SeverityLevel.NORMAL)
+    @Title("check that search works correct")
+    @Description("1) open home page; \n"
             + "2) hover on search field; \n"
             + "3) send search request; \n"
             + "4(check that search works correct. \n")
-    public void test5(String key){
+
+    @Test (dataProvider = "keysForSearch", enabled = true)
+    public void checkForSearch(String key){
         log.info("Log step 1: Try to search");
         oxaHomePage.tryToSearch(key);
 
         log.info("Log step 2: Check that search works correct");
+        makeScreenshot();
         Assert.assertTrue("search works incorrect", searchResultsPage.checkAllPagesForResults(key));
     }
 
-    @Test (enabled = false, description = "1) go to home page; \n"
+    @Features("About Us")
+    @Severity(SeverityLevel.MINOR)
+    @Title("check navigation between pages")
+    @Description("1) go to home page; \n"
             + "2) go to about us; \n"
             + "3) go to contact us; \n"
             + "4) check that page is contact us. \n")
-    public void test6(){
+
+    @Test (enabled = true)
+    public void checkNavigationBetweenPages2(){
         log.info("Log step 1: Go to About As tab");
         oxaHomePage.goToTabAboutAs();
 
@@ -128,11 +169,20 @@ public class AboutUsTest extends BaseTest {
         aboutUsPage.goToContactUsFromAboutUsPage();
 
         log.info("Log step 3: Check that page is Contact Us");
+        makeScreenshot();
         Assert.assertEquals("page isn't contact us", ContactUsPage.CONTACT_US_HEADER_TEXT, contactUsPage.getHeaderText());
     }
 
-    @Test
-    public void test7(){
+
+    @Features("About Us")
+    @Severity(SeverityLevel.MINOR)
+    @Title("check navigation to the customer software services page")
+    @Description("1) go to home page; \n"
+            + "2) go to about us; \n"
+            + "3) go to customer software services page; \n"
+            + "4) check that page is software services page. \n")
+    @Test (enabled = true)
+    public void checkNavigationBetweenPages3(){
         log.info("Log step 1: Go to About As tab");
         oxaHomePage.goToTabAboutAs();
 
@@ -140,11 +190,19 @@ public class AboutUsTest extends BaseTest {
         aboutUsPage.goToCustomerSoftwareServices();
 
         log.info("Log step 3: Check that page is Customer software services page");
+        makeScreenshot();
         Assert.assertEquals("page isn't customer software services", CustomSoftwareDevelopmentServices.CUSTOMER_SOFTWARE_SERVICES_HEADER_TEXT, customSoftwareDevelopmentServices.getHeaderText());
     }
 
-    @Test
-    public void test8(){
+    @Features("About Us")
+    @Severity(SeverityLevel.MINOR)
+    @Title("check navigation to the web development services page")
+    @Description("1) go to home page; \n"
+            + "2) go to about us; \n"
+            + "3) go to web development services page; \n"
+            + "4) check that page is web development services page. \n")
+    @Test (enabled = true)
+    public void checkNavigationBetweenPages4(){
         log.info("Log step 1: Go to About As tab");
         oxaHomePage.goToTabAboutAs();
 
@@ -152,11 +210,20 @@ public class AboutUsTest extends BaseTest {
         aboutUsPage.goToWebDevelopmentServices();
 
         log.info("Log step 3: Check that page is Web development services page");
+        makeScreenshot();
         Assert.assertEquals("page isn't web development services", WebDevelopmentServices.WEB_DEVELOPMENT_SERVICES_HEADER_TEXT, webDevelopmentServices.getHeaderText());
     }
 
-    @Test
-    public void test9(){
+
+    @Features("About Us")
+    @Severity(SeverityLevel.MINOR)
+    @Title("check navigation to the mobile development services page")
+    @Description("1) go to home page; \n"
+            + "2) go to about us; \n"
+            + "3) go to mobile development services page; \n"
+            + "4) check that page is mobile development services page. \n")
+    @Test (enabled = true)
+    public void checkNavigationBetweenPages5(){
         log.info("Log step 1: Go to About As tab");
         oxaHomePage.goToTabAboutAs();
 
@@ -164,11 +231,19 @@ public class AboutUsTest extends BaseTest {
         aboutUsPage.goToMobileApplicationDevelopmentServices();
 
         log.info("Log step 3: Check that page is Mobile development services page");
+        makeScreenshot();
         Assert.assertEquals("page isn't web development services", MobileDevelopmentServices.MOBILE_DEVELOPMENT_SERVICED_HEADER_TEXT, mobileDevelopmentServices.getHeaderText());
     }
 
-    @Test
-    public void test10(){
+    @Features("About Us")
+    @Severity(SeverityLevel.MINOR)
+    @Title("check navigation to the technical support page")
+    @Description("1) go to home page; \n"
+            + "2) go to about us; \n"
+            + "3) go to technical support page; \n"
+            + "4) check that page is technical support page. \n")
+    @Test (enabled = true)
+    public void checkNavigationBetweenPages6(){
         log.info("Log step 1: Go to About As tab");
         oxaHomePage.goToTabAboutAs();
 
@@ -176,6 +251,7 @@ public class AboutUsTest extends BaseTest {
         aboutUsPage.goToTechnicalSupport();
 
         log.info("Log step 3: Check that page is Technical support page");
+        makeScreenshot();
         Assert.assertEquals("page isn't web development services", TechnicalSupportAndMaintenancePage.TECHNICAL_SUPPORT_HEADER_TEXT, technicalSupportAndMaintenancePage.getHeaderText());
     }
 
